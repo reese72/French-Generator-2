21713ce193976553bd5277f142851dc095f5c14f
reese72 <chickendad@icloud.com>
2021-12-20 23:40:43 -0500

    Add project files.

---
+++ French Generator/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace French_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 44);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(458, 238);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(16, 368);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(458, 237);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(898, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(563, 42);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(558, 460);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(133, 22);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Show Translation";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(132, 26);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Output";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 332);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(132, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Translation";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 623);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "French Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ French Generator/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace French_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string l1 = "";
+            string l2 = "";
+            string l3 = "";
+            string l4 = "";
+            string l5 = "";
+            string l6 = "";
+            string l7 = "";
+            string l8 = "";
+            string l9 = "";
+            string test1 = "";
+            Random Random1 = new Random();
+            int selector1 = Random1.Next(5);
+            if (selector1 == 0)
+            {
+                l1 = "Bonjour! \n";
+            }
+            if (selector1 == 1)
+            {
+                l1 = "Enchanté! \n";
+            }
+            if (selector1 == 2)
+            {
+                l1 = "Bonsoir! \n";
+            }
+            if (selector1 == 3)
+            {
+                l1 = "Salut! \n";
+            }
+            if (selector1 == 4)
+            {
+                l1 = "Coucou! \n";
+            }
+            int selector2 = Random1.Next(4);
+            l2 = "Comment ça va? \n";
+            if (selector2 == 0)
+            {
+                l3 = "ça va";
+            }
+            if (selector2 == 1)
+            {
+                l3 = "Comme ci comme ça";
+            }
+            if (selector2 == 2)
+            {
+                l3 = "ça va mal";
+            }
+            if (selector2 == 3)
+            {
+                l3 = "ça va bien";
+            }
+            int selector3 = Random1.Next(4);
+            l2 = "Comment ça va? \n";
+            if (selector3 == 0)
+            {
+                l4 = "ça va \n";
+            }
+            if (selector3 == 1)
+            {
+                l4 = "Comme ci comme ça \n";
+            }
+            if (selector3 == 2)
+            {
+                l4 = "ça va mal \n";
+            }
+            if (selector3 == 3)
+            {
+                l4 = "ça va bien \n";
+            }
+            int selector4 = Random1.Next(5);
+            if (selector4 == 0)
+            {
+                l5 = "Quel âge as-tu? \n";
+            }
+            if (selector4 == 1)
+            {
+                l5 = "Quelle heure est-il? \n";
+            }
+            if (selector4 == 2)
+            {
+                l5 = "Quel jour sommes-nous? \n";
+            }
+            if (selector4 == 3)
+            {
+                l5 = "Comment vous appelez-vous? \n";
+            }
+            if (selector4 == 4)
+            {
+                l5 = "Comment tu t'appelles? \n";
+            }
+           
+            
+            if (selector4 == 0)
+            {
+                A:
+                int selector5 = Random1.Next(30);
+                if (selector5 < 5)
+                {
+                    goto A;
+                }
+                l6 = selector5 + ", et toi? \n";
+            B:
+                l8 = Convert.ToString(Random1.Next(30));
+                int test = Convert.ToInt32(l8);
+                test1 = "J'ai " + Convert.ToString(test) + " ans";
+                if ( test < 5)
+                {
+                    goto B;
+                }
+            }
+            if (selector4 == 1)
+            {
+                C:
+                int test3 = Random1.Next(25);
+                string test2 = Convert.ToString(test3); 
+                if (test3 < 1)
+                {
+                    goto C;
+                }
+                int test4 = Random1.Next(61);
+                string test5 = Convert.ToString(test4);
+                if (test3 == 1)
+                {
+                    l9 = "Il est " + test2 + " heure " +test5;
+                }
+                if (test3 > 1)
+                {
+                    l9 = "Il est " + test2 + " heures " + test5;
+                }
+            }
+            if (selector4 == 2)
+            {
+                l7 = "Quel jour sommes-nous? \n";
+            }
+            if (selector4 == 3)
+            {
+                l7 = "Comment vous appelez-vous? \n";
+            }
+            if (selector4 == 4)
+            {
+                l7 = "Comment tu t'appelles? \n";
+            }
+            richTextBox1.Text = l1 + l2 + l3 + ", et toi? \n" + l4 + l5 + l6 + test1 +l9;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
